6c7ffc300db340110c7dbd36167ae20e8740b524
Kale Shravani <kshravani1803@gmail.com>
2026-06-13 15:09:46 +0530

    Time: 37 ms (24.10%) | Memory: 59.3 MB (45.16%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,24 +1,65 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         Set<List<Integer>> ans = new HashSet<>();
+
+//         for(int i = 0; i < nums.length; i++) {
+//             HashSet<Integer> set = new HashSet<>();
+
+//             for(int j = i + 1; j < nums.length; j++) {
+//                 int required = 0 - nums[i] - nums[j];
+
+//                 if(set.contains(required)) {
+//                     List<Integer> temp = new ArrayList<>();
+//                     temp.add(nums[i]);
+//                     temp.add(nums[j]);
+//                     temp.add(required);
+//                     Collections.sort(temp);
+//                     ans.add(temp);
+//                 }
+//                 set.add(nums[j]);
+//             }
+//         }
+//         return new ArrayList<>(ans);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
+        int sum = 0;
+        int n = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length; i++) {
-            HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < n - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = n - 1;
 
-            for(int j = i + 1; j < nums.length; j++) {
-                int required = 0 - nums[i] - nums[j];
+            sum = -1 * nums[i];
 
-                if(set.contains(required)) {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(required);
-                    Collections.sort(temp);
-                    ans.add(temp);
+            while(left < right) {
+                int s = nums[left] + nums[right];
+
+                if(s == sum) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < n && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while(right >= 0 && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }else if(s < sum) {
+                    left++;
+                }else{
+                    right--;
                 }
-                set.add(nums[j]);
             }
         }
-        return new ArrayList<>(ans);
+        return result;
     }
 }
