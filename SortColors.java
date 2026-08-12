b90ec18b864e595874b3b73c181d08261f38f792
Shravani Kale <kshravani1803@gmail.com>
2026-08-12 09:02:51 +0530

    added dutch national flag algorithm

--- SortColors.java
+++ SortColors.java
@@ -27,7 +27,7 @@ public class SortColors {
      * Time Complexity: O(N)
      * Space Complexity: O(1)
      */
-    static void sortColors(int[] arr) {
+    static void sortColors2(int[] arr) {
 
         // Count the number of 0s, 1s and 2s
         int count0 = 0, count1 = 0, count2 = 0;
@@ -58,6 +58,49 @@ public class SortColors {
              i++) {
             arr[i] = 2;
         }
+    }
+
+     /*
+     * Dutch National Flag Algorithm
+     *
+     * Idea:
+     * Divide the array into 3 sections:
+     * 0s -> left
+     * 1s -> middle
+     * 2s -> right
+     *
+     * low  -> position where next 0 should go
+     * mid  -> current element
+     * high -> position where next 2 should go
+     *
+     * Time Complexity: O(N)
+     * Space Complexity: O(1)
+     */
+
+    static void sortColors(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = 0;
+
+        while (mid <= high) {
+
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            }else if (arr[mid] == 1) {
+                mid++;
+            }else{
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void main(String[] args) {
