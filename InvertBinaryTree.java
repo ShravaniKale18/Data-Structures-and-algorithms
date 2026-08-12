4683cec170a10bffb77a05417d72be859bc6916d
Shravani Kale <kshravani1803@gmail.com>
2026-08-12 09:23:55 +0530

    Leetcode 226: Invert Binary Tree

---
+++ InvertBinaryTree.java
@@ -0,0 +1,47 @@
+/*
+ * Approach: Recursion
+ *
+ * If root == null, return null.
+ * Swap the left and right children.
+ * Recursively invert the left subtree.
+ * Recursively invert the right subtree.
+ * Return root.
+ *
+ * TC => O(N)
+ * SC => O(H)
+ */
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+
+public class InvertBinaryTree {
+
+    public Node invertTree(Node root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
