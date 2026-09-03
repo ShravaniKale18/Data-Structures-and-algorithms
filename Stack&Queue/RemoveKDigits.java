dae5dbd022e863f6469f76d5206c462c08a4209a
Shravani Kale <kshravani1803@gmail.com>
2026-09-03 10:22:35 +0530

    Solve LeetCode 402 Remove K Digits using monotonic stack

---
+++ Stack&Queue/RemoveKDigits.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class RemoveKDigits {
+
+    static String removeKdigits(String num, int k) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < num.length(); i++) {
+            char ch = num.charAt(i);
+
+            while(!stack.isEmpty() && stack.peek() > ch && k > 0) {
+                stack.pop();
+                k--;
+            }
+
+            if(!stack.isEmpty() || ch != '0') {
+                stack.push(ch);
+            }
+        }
+
+        while(stack.size() > 0 && k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch: stack) {
+            if(ch == '0' && sb.length() == 0) {
+                continue;
+            }
+
+            sb.append(ch);
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String num = "1432219";
+        int k = 3;
+        System.out.println(removeKdigits(num, k)); // Output: "1219"    
+    }
+}
