b16f1f931c9e840cb42ab1560e247da6570ba957
ShravaniKale18 <kshravani1803@gmail.com>
2026-09-09 21:57:00 +0530

    Added code for Depth First Traversal Techniques

---
+++ Trees/DFSTraversal.java
@@ -0,0 +1,48 @@
+package Trees;
+
+public class DFSTraversal {
+
+    static void inOrderTraversal(Node head){
+        if (head == null) {
+            return;
+        }
+
+        inOrderTraversal(head.left);
+        System.out.print(head.data+" ");
+        inOrderTraversal(head.right);
+    }
+
+    static void preOrderTraversal(Node head){
+        if (head == null) {
+            return;
+        }
+
+        System.out.print(head.data+" ");
+        preOrderTraversal(head.left);
+        preOrderTraversal(head.right);
+    }
+
+    static void postOrderTraversal(Node head){
+        if (head == null) {
+            return;
+        }
+
+        postOrderTraversal(head.left);
+        postOrderTraversal(head.right);
+        System.out.print(head.data+" ");
+    }
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(50);
+        root.left.right = new Node(100);
+
+        System.out.println("In Order Traversal : ");
+        inOrderTraversal(root);
+        System.out.println("\nPreOrder Traversal : ");
+        preOrderTraversal(root);
+        System.out.println("\nPostOrder Traversal : ");
+        postOrderTraversal(root);
+    }
+}
