8c15fbac1b353fc3a31c6d2c8db2c84ab11d8aa0
ShravaniKale18 <kshravani1803@gmail.com>
2026-09-09 22:18:56 +0530

    Added code Depth First Search

--- Trees/DFSTraversal.java
+++ Trees/DFSTraversal.java
@@ -44,6 +44,7 @@ public class DFSTraversal {
         System.out.print(head.data+" ");
     }
     public static void main(String[] args) {
+        
         Node root = new Node(10);
         root.left = new Node(20);
         root.right = new Node(30);
